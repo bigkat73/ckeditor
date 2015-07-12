0b478c37a1f362a48e085eb65d4a9342ce973a4c
fdk <fdkatona@gmail.com>
2015-07-12 13:10:14 -0600

    update vendor path

--- vendor/assets/javascripts/ckeditor/config.js
+++ vendor/assets/javascripts/ckeditor/config.js
@@ -7,4 +7,15 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+  myFonts = ['Muli'];
+
+  config.font_names = 'sans serif';
+
+  for(var i = 0; i<myFonts.length; i++){
+     config.font_names = config.font_names+';'+myFonts[i];
+     myFonts[i] = 'http://fonts.googleapis.com/css?family='+myFonts[i].replace(' ','+');
+
+     //assuming you use jquery
+     $("head").append("<link rel='stylesheet' href='"+ myFonts[i] +"'>");
+  }
 };
